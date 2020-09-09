dbc621126621ae1be44f2652d1553fb1d6afba2b
DESKTOP-PLG7HV2\Admin <thanhdat.120599@gmail.com>
2020-09-09 16:13:30 +0700

    Update recommendation area shop list page

--- src/app/views/shop/recommendation-area/recommendation-area-list.component.ts
+++ src/app/views/shop/recommendation-area/recommendation-area-list.component.ts
@@ -21,7 +21,7 @@ declare var swal: any;
 export class recommendationAreaListComponent implements OnInit {
   items: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
   itemCount: number = 0;
-  itemFields: any = ['$all', { "category": ["$all", { "thema": ["$all"] }] }, { "events": ["$all"] }];
+  itemFields: any = ['$all', { "user": ["$all"] }, { "category": ["$all", { "thema": ["$all"] }] }, { "events": ["$all"] }];
   query: any = {
     filter: {
       state: { $in: ["APPROVED"] }
@@ -57,6 +57,16 @@ export class recommendationAreaListComponent implements OnInit {
   extra_days: number = 30;
   shop_id: string;
   count_random_20_shop: number;
+  // 
+  expiration_date: any;
+  start_date: any;
+  start_date_1: Date;
+  start_time_1: any = '12:00';
+  expiration_date_1: Date;
+  expiration_time_1: any = '12:00';
+  start_time_unix_timestamp: number;
+  expiration_time_unix_timestamp: number;
+  // 
   @ViewChild('itemsTable') itemsTable: DataTable;
   @ViewChild('fileImage') fileImageElementRef: ElementRef;
 
@@ -121,19 +131,33 @@ export class recommendationAreaListComponent implements OnInit {
   }
   openModalEvent(template: TemplateRef<any>, item) {
     this.shop_id_event = item.id
+    this.expiration_date = item.expired_date
+    this.start_date = item.start_date
     if (item.events.length === 0) {
       this.title_event = null;
       this.description = null;
       this.images_event = [];
-      this.value_of_day = [this.currentDate, this.currentDate];
+      // this.value_of_day = [this.currentDate, this.currentDate];
       this.event_id = null
+      // 
+      this.start_date_1 = null;
+      this.expiration_date_1 = null;
+      this.start_time_1 = '12:00';
+      this.expiration_time_1 = '12:00';
+      // 
     } else {
       this.title_event = item.events[0].title
       this.description = item.events[0].description
-      this.value_of_day = [new Date(parseInt(item.events[0].start_time)), new Date(parseInt(item.events[0].end_time))];
+      // this.value_of_day = [new Date(parseInt(item.events[0].start_time)), new Date(parseInt(item.events[0].end_time))];
       this.images_event = item.events[0].images
       this.event_id = item.events[0].id
       this.state = item.events[0].state
+      // 
+      this.start_date_1 = new Date(parseInt(item.events[0].start_time));
+      this.expiration_date_1 = new Date(parseInt(item.events[0].end_time));
+      this.start_time_1 =moment(parseInt(item.events[0].start_time)).format("HH:mm")
+      this.expiration_time_1 = moment(parseInt(item.events[0].end_time)).format("HH:mm")
+      // 
     }
     this.modalRef = this.modalService.show(template);
   }
@@ -158,18 +182,69 @@ export class recommendationAreaListComponent implements OnInit {
     });
   }
   async submitAddEvent(form: NgForm) {
-    this.submitting = true;
-    if (form.valid) {
-      if (!this.event_id) {
-        await this.addItem(form);
-      } else {
-        await this.updateItem(form);
+    let start_hour = 0;
+    let start_minute = 0;
+    let expiration_hour = 0;
+    let expiration_minute = 0;
+    if (this.start_time_1.split(':').length > 0) {
+      start_hour = this.start_time_1.split(':')[0];
+    }
+    if (this.start_time_1.split(':').length > 1) {
+      start_minute = this.start_time_1.split(':')[1];
+    }
+    if (this.expiration_time_1.split(':').length > 0) {
+      expiration_hour = this.expiration_time_1.split(':')[0];
+    }
+    if (this.expiration_time_1.split(':').length > 1) {
+      expiration_minute = this.expiration_time_1.split(':')[1];
+    }
+    console.log('213123123123123',this.start_time_unix_timestamp)
+    console.log('@@@@@@@',this.start_date)
+    console.log('########',this.expiration_date)
+    this.start_time_unix_timestamp = new Date(this.start_date_1.getFullYear(), this.start_date_1.getMonth(),
+      this.start_date_1.getDate(), start_hour, start_minute, 0).getTime();
+    this.expiration_time_unix_timestamp = new Date(this.expiration_date_1.getFullYear(), this.expiration_date_1.getMonth(),
+      this.expiration_date_1.getDate(), expiration_hour, expiration_minute, 0).getTime();
+    if(this.start_time_unix_timestamp >= this.expiration_time_unix_timestamp){
+      this.alertFailedStartEndTime()
+      this.submitting = false;
+      return;
+    }
+    // 
+    // if (time_end <= exp_date || time_start >= str_date) {
+    if (this.expiration_time_unix_timestamp <= this.expiration_date && this.start_time_unix_timestamp >= this.start_date) {
+      this.submitting = true;
+      console.log(form)
+      if (form.valid) {
+        if (!this.event_id) {
+          await this.addItem(form);
+        } else {
+          await this.updateItem(form);
 
+        }
+      } else {
+        this.alertFormNotValid();
+        this.submitting = false;
       }
     } else {
-      this.alertFormNotValid();
-      this.submitting = false;
+      this.alertFailedDate()
     }
+  }
+  alertFailedStartEndTime() {
+    // this.value_of_day = [moment().format('YYYY-MM-DD'), moment().format('YYYY-MM-DD')]
+    return swal({
+      title: (this.configService.lang === 'en') ? 'Start Time must be earlier than End time' : ((this.configService.lang === 'vn') ? 'Thời gian bắt đầu sự kiện phải sớm hơn thời gian kết thúc sự kiện' : '시작시간은  종료시간보다 늦을 수 없습니다.'),
+      type: 'warning',
+      timer: 3000,
+    });
+  }
+  alertFailedDate() {
+    // this.value_of_day = [moment().format('YYYY-MM-DD'), moment().format('YYYY-MM-DD')]
+    return swal({
+      title: (this.configService.lang === 'en') ? 'The event period is available only within advertising period of relevant shop' : ((this.configService.lang === 'vn') ? 'Thời gian sự kiện chỉ có sẵn trong khoảng thời gian quảng cáo của cửa hàng có liên quan' : '이벤트 기간은  관련 상점 광고기간 내에만 설정가능합니다.'),
+      type: 'warning',
+      timer: 3000,
+    });
   }
   async addItem(form: NgForm) {
     try {
@@ -178,8 +253,10 @@ export class recommendationAreaListComponent implements OnInit {
         description: this.description,
         images: this.images_event,
         state: "APPROVED",
-        start_time: moment(this.value_of_day[0]).valueOf(),
-        end_time: moment(this.value_of_day[1]).valueOf(),
+        start_time: this.start_time_unix_timestamp,
+        end_time: this.expiration_time_unix_timestamp,
+        // start_time: moment(this.value_of_day[0]).valueOf(),
+        // end_time: moment(this.value_of_day[1]).valueOf(),
         shop_id: this.shop_id_event,
         created_by_admin: true
       });
@@ -199,8 +276,10 @@ export class recommendationAreaListComponent implements OnInit {
         description: this.description,
         images: this.images_event,
         // state: "APPROVED",
-        start_time: moment(this.value_of_day[0]).valueOf(),
-        end_time: moment(this.value_of_day[1]).valueOf(),
+        start_time: this.start_time_unix_timestamp,
+        end_time: this.expiration_time_unix_timestamp,
+        // start_time: moment(this.value_of_day[0]).valueOf(),
+        // end_time: moment(this.value_of_day[1]).valueOf(),
         shop_id: this.shop_id_event,
         created_by_admin: true
       });
@@ -209,7 +288,14 @@ export class recommendationAreaListComponent implements OnInit {
       this.submitting = false;
       this.itemsTable.reloadItems();
     } catch (error) {
-      this.alertErrorFromServer(error.error.message);
+      if (error.error.message === 'your event end time cannot bigger than shop expiration date.') {
+        const msg = (this.configService.lang === 'en') ? 'The end time of event can\'t be later than the end time of Post.'
+          : ((this.configService.lang === 'vn') ? 'Thời gian kết thúc sự kiện không thể trễ hơn thời gian kết thúc của bài đăng' : '이벤트만료일은  상점만료일을 초과하여 설정할 수  없습니다.')
+        this.alertErrorFromServer(msg);
+      } else {
+        this.alertErrorFromServer(error.error.message);
+        console.log(error.error.message);
+      }
       this.submitting = false;
     }
   }
@@ -479,13 +565,49 @@ export class recommendationAreaListComponent implements OnInit {
       console.log('error: ', error);
     }
   }
-
+  // ////////////////
   getDateFromUnixTimestamp(unixtimestamp: any) {
-    return moment(parseInt(unixtimestamp)).format("YYYY-MM-DD")
+    return moment(parseInt(unixtimestamp)).format("YYYY-MM-DD hh:mm A")
   }
   mathRemainingTime(unixtimestamp: any) {
-    return Math.floor((unixtimestamp - moment().valueOf()) / (24 * 60 * 60 * 1000))
+    // return new Date(parseInt(unixtimestamp));
+    return (unixtimestamp - moment().valueOf()) / (24 * 60 * 60 * 1000)
   }
+  ceilRemainingTime(unixtimestamp: any){
+    return Math.ceil((unixtimestamp - moment().valueOf()) / (24 * 60 * 60 * 1000))
+  }
+  openModalSubTime(template: TemplateRef<any>, item) {
+    this.id_update = item.id
+    this.expired_date = item.expired_date;
+    this.modalRef = this.modalService.show(template);
+  }
+  async submitSubTime(form: NgForm) {
+    this.submitting = true;
+    if (form.valid) {
+      try {
+        const max_day = Math.ceil((this.expired_date - moment().valueOf()) / 86400000)
+        console.log("@@@@ n ",max_day)
+        if(this.extra_days > max_day){
+          this.extra_days = max_day
+        }
+        await this.apiService.shop.update(this.id_update, {
+          expired_date: parseInt(this.expired_date.toString()) - (this.extra_days * 86400000)
+        });
+
+        this.alertSuccess();
+        this.modalRef.hide()
+        this.submitting = false;
+        this.itemsTable.reloadItems();
+      } catch (error) {
+        this.alertErrorFromServer(error.error.message);
+        this.submitting = false;
+      }
+    } else {
+      this.alertFormNotValid();
+      this.submitting = false;
+    }
+  }
+  // /////////////
   subTimeOpen1(time) {
     return time.substring(0, time.indexOf("~"))
   }
